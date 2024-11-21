db27a9e41e3a9c7ca753ba7db3ef8542d6f72c98
eliss <staaf.elis@gmail.com>
2024-11-21 20:41:53 +0100

    fmake stat

--- gen.sh
+++ gen.sh
@@ -6,10 +6,10 @@ if [ ! -d $WORKDIR ]; then
     mkdir "$WORKDIR"
 fi
 
-echo "[INFO]: Generating $WORKDIR/FMakefile"
+echo "fmake: Generating $WORKDIR/FMakefile"
 echo "require $2" >> "$WORKDIR/FMakefile"
 echo "set PAKG_VERSION \"1.0.0\"" >> "$WORKDIR/FMakefile"
 echo "set PAKG_NAME $(basename "$WORKDIR")" >> "$WORKDIR/FMakefile"
 echo "println \"\$PAKG_NAME -- Version \$PAKG_VERSION\"" >> "$WORKDIR/FMakefile"
 
-echo "[INFO]: Project generation was a success!"
+echo "fmake: Project generation was a success!"
